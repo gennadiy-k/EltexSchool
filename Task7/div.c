f2d19a6b3c4d5e47be7d446ef72765a9ef62f570
gannadiy-k <servis227@gmail.com>
2026-03-14 04:18:06 +0700

    Add Task7

---
+++ Task7/div.c
@@ -0,0 +1,3 @@
+int div(int var1, int var2) {
+    return var1 / var2;
+}
---
+++ Task7/math_ops.h
@@ -0,0 +1,9 @@
+#ifndef math_ops
+#define math_ops
+
+int add(int, int);
+int sub(int, int);
+int mul(int, int);
+int div(int, int);
+
+#endif
